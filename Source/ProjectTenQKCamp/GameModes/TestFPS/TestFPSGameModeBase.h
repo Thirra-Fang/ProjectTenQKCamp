3814ecc608772639610438f6fcf1e9af2fc1f813
Thirra Fang <hh1985582256@163.com>
2026-01-05 20:38:17 +0800

    feat:新建GameMode类和Character蓝图类

---
+++ Source/ProjectTenQKCamp/GameModes/TestFPS/TestFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTENQKCAMP_API ATestFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
